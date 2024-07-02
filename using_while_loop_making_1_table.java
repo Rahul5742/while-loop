63928b6d7ec6517cf52f89327c18dcf95874a8dd
Rahul5742 <rkgamingchannel8@gmail.com>
2024-07-02 21:32:10 +0545

    using_while_loop_making_1_table
    2 kaa  table

---
+++ using_while_loop_making_1_table.java
@@ -0,0 +1,11 @@
+public class using_while_loop_making_1_table {
+    public static void main(String[] args) {
+        int a=1;
+        while(a<=10){
+            int sum=a*2;
+            System.out.println("2 * "+a+" = "+sum);
+            a++;
+        }
+
+    }
+}
